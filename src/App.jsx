aa66ca100f420ff13e2e4a5135a28be1bd63762d
Rupesh3000 <rupeshpatil11121@gmail.com>
2024-12-20 18:16:21 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,29 +6,48 @@ import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } 
 import Layout from './Layout.jsx'
 import Home from './Reactrouter/Home/Home.jsx'
 import About from './Reactrouter/About/About.jsx'
+import Contact from './Reactrouter/Contact/Contact.jsx'
+import User from './Reactrouter/User/User.jsx'
 
 
 
 function App() {
 
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Layout />,
-      children: [
-        {
-          path: "",
-          element: <Home />
-        },
-        {
-          path: "about",
-          element: <About />
-        }
+  // const router = createBrowserRouter([
+  //   {
+  //     path: '/',
+  //     element: <Layout />,
+  //     children: [
+  //       {
+  //         path: "",
+  //         element: <Home />
+  //       },
+  //       {
+  //         path: "about",
+  //         element: <About />
+  //       },
+  //       {
+  //         path: 'contact',
+  //         element: <Contact />
+  //       }
 
-      ]
-    }
-  ])
 
+  //     ]
+  //   }
+  // ])
+
+
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout />}>
+        <Route path='' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        {/* <Route path='user/:userId' element={<User />} /> */}
+      </Route>
+    )
+  )
   return (
     <>
       <RouterProvider router={router} />
